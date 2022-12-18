2519bc0781b03051ecc5fc4634cda3805e511e3d
FrozenTrojan <ethanton@csu.fullerton.edu>
2022-12-17 16:23:05 -0800

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -131,10 +131,10 @@ export default function About() {
                   </h1>
                 </div>
                 <p className="transition duration-200 ease-linear text-neutral-50 hover:text-slate-900 text-center">
-                  "Add what you did"
+                  "Styles and graphics for the website"
                 </p>
                 <p className="text-neutral-50 italic w-64 h-full transition duration-200 ease-linear hover:text-slate-900 text-center text-sm">
-                  "Quotes?"
+                  "Difficult Project"
                 </p>
               </div>
               <a
