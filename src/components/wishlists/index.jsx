24ec5803dbbe92eba4725be4cb64cdd75443dcd0
FrozenTrojan <ethanton@csu.fullerton.edu>
2022-12-17 16:12:38 -0800

    Update index.jsx

--- src/components/wishlists/index.jsx
+++ src/components/wishlists/index.jsx
@@ -79,7 +79,7 @@ export default function Wishlists() {
 							type="submit"
 							class="px-6 py-2 mt-4 text-white bg-green-600 rounded-lg hover:bg-green-900"
 						>
-							Save
+							Saved
 						</button>
 					</div>
 				</div>
